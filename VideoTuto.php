37e9e64c4547a655183f9b3ce5e6e1a6be8913f6
archiGenie <145983196+archiGenie@users.noreply.github.com>
2023-10-27 23:52:28 +0100

    Add files via upload

---
+++ VideoTuto.php
@@ -0,0 +1,306 @@
+{
+  "response": "success",
+  "row_count": 19,
+  "data": [
+    {
+      "Id": 0,
+      "App_Name": "LA RECETTE DU SOLDAT - contes populaires slaves",
+      "URL": "https://www.youtube.com/watch?v=_1cr82OFgJw",
+      "App_Icon": "https://i.ytimg.com/vi/_1cr82OFgJw/hqdefault.jpg"
+    },
+    {
+      "Id": 1,
+      "App_Name": "LE NIGAUD - contes populaires slaves",
+      "URL": "https://www.youtube.com/watch?v=hXNG137SL8c",
+      "App_Icon": "https://i.ytimg.com/vi/hXNG137SL8c/hqdefault.jpg"
+    },
+    {
+      "Id": 2,
+      "App_Name": "LE CHAT ET LE RENARD - contes populaires slaves",
+      "URL": "https://www.youtube.com/watch?v=oTLXEuWhhCU",
+      "App_Icon": "https://i.ytimg.com/vi/oTLXEuWhhCU/hqdefault.jpg"
+    },
+    {
+      "Id": 3,
+      "App_Name": "Private video",
+      "URL": "https://www.youtube.com/watch?v=u_2Q74EdI4U",
+      "App_Icon": "https://i.ytimg.com/vi/u_2Q74EdI4U/hqdefault.jpg"
+    },
+    {
+      "Id": 4,
+      "App_Name": "Private video",
+      "URL": "https://www.youtube.com/watch?v=GaDPQfD2HD8",
+      "App_Icon": "https://i.ytimg.com/vi/GaDPQfD2HD8/hqdefault.jpg"
+    },
+    {
+      "Id": 5,
+      "App_Name": "Private video",
+      "URL": "https://www.youtube.com/watch?v=W67rzTXzPtI",
+      "App_Icon": "https://i.ytimg.com/vi/W67rzTXzPtI/hqdefault.jpg"
+    },
+    {
+      "Id": 6,
+      "App_Name": "Private video",
+      "URL": "https://www.youtube.com/watch?v=0Dwgxq3_VhU",
+      "App_Icon": "https://i.ytimg.com/vi/0Dwgxq3_VhU/hqdefault.jpg"
+    },
+    {
+      "Id": 7,
+      "App_Name": "Private video",
+      "URL": "https://www.youtube.com/watch?v=4g1kXNag-Z0",
+      "App_Icon": "https://i.ytimg.com/vi/4g1kXNag-Z0/hqdefault.jpg"
+    },
+    {
+      "Id": 8,
+      "App_Name": "19 - L’ŒIL QUI RIT ET L’ŒIL QUI PLEURE OU LE RENARD BOITEUX  - contes populaires slaves",
+      "URL": "https://www.youtube.com/watch?v=BIpE1LJRna8",
+      "App_Icon": "https://i.ytimg.com/vi/BIpE1LJRna8/hqdefault.jpg"
+    },
+    {
+      "Id": 9,
+      "App_Name": "18 - LE LOUP NIGAUD - contes populaires slaves",
+      "URL": "https://www.youtube.com/watch?v=1zmGepJGIOc",
+      "App_Icon": "https://i.ytimg.com/vi/1zmGepJGIOc/hqdefault.jpg"
+    },
+    {
+      "Id": 10,
+      "App_Name": "16 - LE POISSON D’OR - contes populaires slaves",
+      "URL": "https://www.youtube.com/watch?v=z33v1dAXfsQ",
+      "App_Icon": "https://i.ytimg.com/vi/z33v1dAXfsQ/hqdefault.jpg"
+    },
+    {
+      "Id": 11,
+      "App_Name": "20 - LA BAVARDE - contes populaires slaves",
+      "URL": "https://www.youtube.com/watch?v=l-tzHiQEz5g",
+      "App_Icon": "https://i.ytimg.com/vi/l-tzHiQEz5g/hqdefault.jpg"
+    },
+    {
+      "Id": 12,
+      "App_Name": "11 - LE LANGAGE  DES ANIMAUX - contes populaires slaves",
+      "URL": "https://www.youtube.com/watch?v=Db_hxihptcE",
+      "App_Icon": "https://i.ytimg.com/vi/Db_hxihptcE/hqdefault.jpg"
+    },
+    {
+      "Id": 13,
+      "App_Name": "10 - LA BABA IAGA - contes populaires slaves",
+      "URL": "https://www.youtube.com/watch?v=ajtux9udVK4",
+      "App_Icon": "https://i.ytimg.com/vi/ajtux9udVK4/hqdefault.jpg"
+    },
+    {
+      "Id": 14,
+      "App_Name": "9 - BLANCHE NEIGE - contes populaires slaves",
+      "URL": "https://www.youtube.com/watch?v=VNuq7Ha1E8A",
+      "App_Icon": "https://i.ytimg.com/vi/VNuq7Ha1E8A/hqdefault.jpg"
+    },
+    {
+      "Id": 15,
+      "App_Name": "8 - LE PRINCE INESPÉRÉ - contes populaires slaves",
+      "URL": "https://www.youtube.com/watch?v=EFISFLpiZSo",
+      "App_Icon": "https://i.ytimg.com/vi/EFISFLpiZSo/hqdefault.jpg"
+    },
+    {
+      "Id": 16,
+      "App_Name": "7 - LE BERGER ET LE DRAGON  - Contes populaires slaves",
+      "URL": "https://www.youtube.com/watch?v=nexTFMgSU68",
+      "App_Icon": "https://i.ytimg.com/vi/nexTFMgSU68/hqdefault.jpg"
+    },
+    {
+      "Id": 17,
+      "App_Name": "6 - LA GELÉE LE SOLEIL ET LE VENT - contes populaires slaves",
+      "URL": "https://www.youtube.com/watch?v=zltIr3OrX7s",
+      "App_Icon": "https://i.ytimg.com/vi/zltIr3OrX7s/hqdefault.jpg"
+    },
+    {
+      "Id": 18,
+      "App_Name": "1- UNE DRACHME DE LANGUE - contes populaires slaves",
+      "URL": "https://www.youtube.com/watch?v=Ibe_mp6X9BQ",
+      "App_Icon": "https://i.ytimg.com/vi/Ibe_mp6X9BQ/hqdefault.jpg"
+    },
+    {
+      "Id": 19,
+      "App_Name": "2- LA TABLE, LA MUSETTE ET LE SAC - contes populaires slaves",
+      "URL": "https://www.youtube.com/watch?v=mWq5dG6XOw4",
+      "App_Icon": "https://i.ytimg.com/vi/mWq5dG6XOw4/hqdefault.jpg"
+    },
+    {
+      "Id": 20,
+      "App_Name": "3 - LE PETIT POUCET RUSSE - contes populaires slaves",
+      "URL": "https://www.youtube.com/watch?v=N66siE6dvec",
+      "App_Icon": "https://i.ytimg.com/vi/N66siE6dvec/hqdefault.jpg"
+    },
+    {
+      "Id": 21,
+      "App_Name": "4 - LA MAUVAISE FEMME  - contes populaires slaves",
+      "URL": "https://www.youtube.com/watch?v=eY6W3jyGo1A",
+      "App_Icon": "https://i.ytimg.com/vi/eY6W3jyGo1A/hqdefault.jpg"
+    },
+    {
+      "Id": 22,
+      "App_Name": "1- L’Appel de Cthulhu  - Chapitre I - H P Lovecraft -  Texte + Livre  Audio français",
+      "URL": "https://www.youtube.com/watch?v=tSabXBm1UrY",
+      "App_Icon": "https://i.ytimg.com/vi/tSabXBm1UrY/hqdefault.jpg"
+    },
+    {
+      "Id": 23,
+      "App_Name": "2- L’Appel de Cthulhu - Chapitre II- H.P. Lovecraft - Texte + Livre Audio français",
+      "URL": "https://www.youtube.com/watch?v=DrJN3fCsqgc",
+      "App_Icon": "https://i.ytimg.com/vi/DrJN3fCsqgc/hqdefault.jpg"
+    },
+    {
+      "Id": 24,
+      "App_Name": "3 - L’Appel de Cthulhu  - Chapitre III - H P  Lovecraft  - Texte + Livre  Audio français",
+      "URL": "https://www.youtube.com/watch?v=ztK0Y2OuHz8",
+      "App_Icon": "https://i.ytimg.com/vi/ztK0Y2OuHz8/hqdefault.jpg"
+    },
+    {
+      "Id": 25,
+      "App_Name": "5 - LA FILLE  DU DOGE - contes populaires slaves",
+      "URL": "https://www.youtube.com/watch?v=97mSwj_Qd4A",
+      "App_Icon": "https://i.ytimg.com/vi/97mSwj_Qd4A/hqdefault.jpg"
+    },
+    {
+      "Id": 26,
+      "App_Name": "Private video",
+      "URL": "https://www.youtube.com/watch?v=-zyHVCLVRQU",
+      "App_Icon": "https://i.ytimg.com/vi/-zyHVCLVRQU/hqdefault.jpg"
+    },
+    {
+      "Id": 27,
+      "App_Name": "Trois contes sélectionnés (Grimm) : \"L'aïeul et le petit-fils\", \"Les trois fainéants\" et \"Le clou\"",
+      "URL": "https://www.youtube.com/watch?v=fDDA2cIwbMs",
+      "App_Icon": "https://i.ytimg.com/vi/fDDA2cIwbMs/hqdefault.jpg"
+    },
+    {
+      "Id": 28,
+      "App_Name": "Les Trois Frères : Une Histoire d'Adresse et d'Amitié | Histoire audio",
+      "URL": "https://www.youtube.com/watch?v=gITUhuqgsD8",
+      "App_Icon": "https://i.ytimg.com/vi/gITUhuqgsD8/hqdefault.jpg"
+    },
+    {
+      "Id": 29,
+      "App_Name": "La Chouette : Une Histoire d'Intrigue et de Peur | Livre Audio + Texte",
+      "URL": "https://www.youtube.com/watch?v=eUpPo4dioFE",
+      "App_Icon": "https://i.ytimg.com/vi/eUpPo4dioFE/hqdefault.jpg"
+    },
+    {
+      "Id": 30,
+      "App_Name": "Le Loup et le Renard - Un conte sur l'avidité et la ruse | Histoire Audio + Texte",
+      "URL": "https://www.youtube.com/watch?v=b5-S0S902tM",
+      "App_Icon": "https://i.ytimg.com/vi/b5-S0S902tM/hqdefault.jpg"
+    },
+    {
+      "Id": 31,
+      "App_Name": "La Douce Bouillie - Un conte sur les conséquences inattendues d'un petit pot magique",
+      "URL": "https://www.youtube.com/watch?v=2i2280_k_5Y",
+      "App_Icon": "https://i.ytimg.com/vi/2i2280_k_5Y/hqdefault.jpg"
+    },
+    {
+      "Id": 32,
+      "App_Name": "Le Docteur Universel - Une histoire pleine de malentendus et de ruses",
+      "URL": "https://www.youtube.com/watch?v=1YRhGB9WJ1Y",
+      "App_Icon": "https://i.ytimg.com/vi/1YRhGB9WJ1Y/hqdefault.jpg"
+    },
+    {
+      "Id": 33,
+      "App_Name": "Le Renard et le Chat : Une Aventure Astucieuse !",
+      "URL": "https://www.youtube.com/watch?v=1ZO7GjjUlNI",
+      "App_Icon": "https://i.ytimg.com/vi/1ZO7GjjUlNI/hqdefault.jpg"
+    },
+    {
+      "Id": 34,
+      "App_Name": "Le Renard et les Oies : Un Conte Amusant et Surprenant ! |",
+      "URL": "https://www.youtube.com/watch?v=Eh-c1MbQTuk",
+      "App_Icon": "https://i.ytimg.com/vi/Eh-c1MbQTuk/hqdefault.jpg"
+    },
+    {
+      "Id": 35,
+      "App_Name": "Le Violon Merveilleux   Une Aventure Musicale dans la Forêt Enchantée !",
+      "URL": "https://www.youtube.com/watch?v=DPrzkIFAtG0",
+      "App_Icon": "https://i.ytimg.com/vi/DPrzkIFAtG0/hqdefault.jpg"
+    },
+    {
+      "Id": 36,
+      "App_Name": "Le Loup et l'Homme | Une aventure pleine de surprises et de leçons !",
+      "URL": "https://www.youtube.com/watch?v=rjzoTLRcowc",
+      "App_Icon": "https://i.ytimg.com/vi/rjzoTLRcowc/hqdefault.jpg"
+    },
+    {
+      "Id": 37,
+      "App_Name": "Il était une fois un amour entre un oiseau et une baleine",
+      "URL": "https://www.youtube.com/watch?v=vcy2Beyguog",
+      "App_Icon": "https://i.ytimg.com/vi/vcy2Beyguog/hqdefault.jpg"
+    },
+    {
+      "Id": 38,
+      "App_Name": "Chassez le naturel | Le Duel des Tics : Le Lièvre et le Singe",
+      "URL": "https://www.youtube.com/watch?v=e73pzqOm4dw",
+      "App_Icon": "https://i.ytimg.com/vi/e73pzqOm4dw/hqdefault.jpg"
+    },
+    {
+      "Id": 39,
+      "App_Name": "Le Conte du Chat Astucieux | Contes et légendes | Histoire audio avec texte",
+      "URL": "https://www.youtube.com/watch?v=UES510V1d4U",
+      "App_Icon": "https://i.ytimg.com/vi/UES510V1d4U/hqdefault.jpg"
+    },
+    {
+      "Id": 40,
+      "App_Name": "Belle comme la lune | Une histoire d'amour et de beauté | Conte audio avec texte",
+      "URL": "https://www.youtube.com/watch?v=31XFZSBIlA0",
+      "App_Icon": "https://i.ytimg.com/vi/31XFZSBIlA0/hqdefault.jpg"
+    },
+    {
+      "Id": 41,
+      "App_Name": "Balajoudh et l'ogresse Tseriel |  Une histoire audio  captivante",
+      "URL": "https://www.youtube.com/watch?v=uutsy2JWxnk",
+      "App_Icon": "https://i.ytimg.com/vi/uutsy2JWxnk/hqdefault.jpg"
+    },
+    {
+      "Id": 42,
+      "App_Name": "Blanche-Neige et les sept nains| Une histoire audio avec texte envoûtante et captivante",
+      "URL": "https://www.youtube.com/watch?v=gvp5ekOtTjM",
+      "App_Icon": "https://i.ytimg.com/vi/gvp5ekOtTjM/hqdefault.jpg"
+    },
+    {
+      "Id": 43,
+      "App_Name": "Celui qui n'en faisait qu'à sa tête | Un conte captivant avec audio et texte",
+      "URL": "https://www.youtube.com/watch?v=Hjynlp9Lv5I",
+      "App_Icon": "https://i.ytimg.com/vi/Hjynlp9Lv5I/hqdefault.jpg"
+    },
+    {
+      "Id": 44,
+      "App_Name": "Celle qui ne voulait pas se marier | Histoire audio captivante | Audio + Texte | Contes et légendes",
+      "URL": "https://www.youtube.com/watch?v=eeoRqyhzRcw",
+      "App_Icon": "https://i.ytimg.com/vi/eeoRqyhzRcw/hqdefault.jpg"
+    },
+    {
+      "Id": 45,
+      "App_Name": "Bâton, tape ! | Une histoire audio captivante | Le Bâton Magique",
+      "URL": "https://www.youtube.com/watch?v=OZT7yt_bui0",
+      "App_Icon": "https://i.ytimg.com/vi/OZT7yt_bui0/hqdefault.jpg"
+    },
+    {
+      "Id": 46,
+      "App_Name": "BABA YAGA - Une histoire captivante de la campagne russe | Livre Audio + Texte | Contes et légendes",
+      "URL": "https://www.youtube.com/watch?v=pKkmB6JLxmE",
+      "App_Icon": "https://i.ytimg.com/vi/pKkmB6JLxmE/hqdefault.jpg"
+    },
+    {
+      "Id": 47,
+      "App_Name": "Aventures Dangereuses | Livre Audio + Texte | Contes et légendes",
+      "URL": "https://www.youtube.com/watch?v=ZPkRa8Vq6yQ",
+      "App_Icon": "https://i.ytimg.com/vi/ZPkRa8Vq6yQ/hqdefault.jpg"
+    },
+    {
+      "Id": 48,
+      "App_Name": "Le Voyage Extraordinaire de Mamadou | Quand l'Apprentissage ouvre les Portes du Monde Invisible",
+      "URL": "https://www.youtube.com/watch?v=nj5J-AtPEGA",
+      "App_Icon": "https://i.ytimg.com/vi/nj5J-AtPEGA/hqdefault.jpg"
+    },
+    {
+      "Id": 49,
+      "App_Name": "Le Petit Chaperon rouge - Charles Perrault - Livre Audio Français + Texte",
+      "URL": "https://www.youtube.com/watch?v=4y0-32u04i0",
+      "App_Icon": "https://i.ytimg.com/vi/4y0-32u04i0/hqdefault.jpg"
+    }
+  ]
+}
