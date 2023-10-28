1da7d0d890e559cef8ba7f492f5c880d1e46bcd2
archiGenie <145983196+archiGenie@users.noreply.github.com>
2023-10-28 01:52:51 +0100

    Update VideoTuto.php

--- VideoTuto.php
+++ VideoTuto.php
@@ -10,31 +10,31 @@
     },
     {
       "Id": 1,
-      "App_Name": "Cendrillon de Jacob et Wilhelm Grimm - Livre Audio + Texte -",
+      "App_Name": "Cendrillon de Jacob et Wilhelm Grimm ",
       "URL": "https://www.youtube.com/watch?v=eayNkBudqnU",
       "App_Icon": "https://i.ytimg.com/vi/eayNkBudqnU/hqdefault.jpg"
     },
     {
       "Id": 2,
-      "App_Name": "Le petit chaperon rouge de Jacob et Wilhelm Grimm - Livre Audio + Texte -",
+      "App_Name": "Le petit chaperon rouge de Jacob et Wilhelm Grimm ",
       "URL": "https://www.youtube.com/watch?v=zf39n76aqgA",
       "App_Icon": "https://i.ytimg.com/vi/zf39n76aqgA/hqdefault.jpg"
     },
     {
       "Id": 3,
-      "App_Name": "Le loup et les sept chevreaux de Jacob et Wilhelm Grimm - Livre Audio + Texte -",
+      "App_Name": "Le loup et les sept chevreaux de Jacob et Wilhelm Grimm ",
       "URL": "https://www.youtube.com/watch?v=8Hm4nzhqXjo",
       "App_Icon": "https://i.ytimg.com/vi/8Hm4nzhqXjo/hqdefault.jpg"
     },
     {
       "Id": 4,
-      "App_Name": "La princesse sur les pois de Jacob et Wilhelm Grimm - Livre Audio + Texte -",
+      "App_Name": "La princesse sur les pois de Jacob et Wilhelm Grimm ",
       "URL": "https://www.youtube.com/watch?v=8o6aJCWZSlw",
       "App_Icon": "https://i.ytimg.com/vi/8o6aJCWZSlw/hqdefault.jpg"
     },
     {
       "Id": 5,
-      "App_Name": "5 - LA FILLE  DU DOGE - contes populaires slaves",
+      "App_Name": "5 - LA FILLE  DU DOGE",
       "URL": "https://www.youtube.com/watch?v=97mSwj_Qd4A",
       "App_Icon": "https://i.ytimg.com/vi/97mSwj_Qd4A/hqdefault.jpg"
     },
@@ -52,19 +52,19 @@
     },
     {
       "Id": 8,
-      "App_Name": "Les Trois Frères : Une Histoire d'Adresse et d'Amitié | Histoire audio",
+      "App_Name": "Les Trois Frères : Une Histoire d'Adresse et d'Amitié",
       "URL": "https://www.youtube.com/watch?v=gITUhuqgsD8",
       "App_Icon": "https://i.ytimg.com/vi/gITUhuqgsD8/hqdefault.jpg"
     },
     {
       "Id": 9,
-      "App_Name": "La Chouette : Une Histoire d'Intrigue et de Peur | Livre Audio + Texte",
+      "App_Name": "La Chouette : Une Histoire d'Intrigue et de Peur ",
       "URL": "https://www.youtube.com/watch?v=eUpPo4dioFE",
       "App_Icon": "https://i.ytimg.com/vi/eUpPo4dioFE/hqdefault.jpg"
     },
     {
       "Id": 10,
-      "App_Name": "Le Loup et le Renard - Un conte sur l'avidité et la ruse | Histoire Audio + Texte",
+      "App_Name": "Le Loup et le Renard - Un conte sur l'avidité et la ruse ",
       "URL": "https://www.youtube.com/watch?v=b5-S0S902tM",
       "App_Icon": "https://i.ytimg.com/vi/b5-S0S902tM/hqdefault.jpg"
     },
@@ -118,13 +118,13 @@
     },
     {
       "Id": 19,
-      "App_Name": "Le Conte du Chat Astucieux | Contes et légendes | Histoire audio avec texte",
+      "App_Name": "Le Conte du Chat Astucieux ",
       "URL": "https://www.youtube.com/watch?v=UES510V1d4U",
       "App_Icon": "https://i.ytimg.com/vi/UES510V1d4U/hqdefault.jpg"
     },
     {
       "Id": 20,
-      "App_Name": "Belle comme la lune | Une histoire d'amour et de beauté | Conte audio avec texte",
+      "App_Name": "Belle comme la lune | Une histoire d'amour et de beauté ",
       "URL": "https://www.youtube.com/watch?v=31XFZSBIlA0",
       "App_Icon": "https://i.ytimg.com/vi/31XFZSBIlA0/hqdefault.jpg"
     },
@@ -148,7 +148,7 @@
     },
     {
       "Id": 24,
-      "App_Name": "Celle qui ne voulait pas se marier | Histoire audio captivante | Audio + Texte | Contes et légendes",
+      "App_Name": "Celle qui ne voulait pas se marier | Histoire audio captivante ",
       "URL": "https://www.youtube.com/watch?v=eeoRqyhzRcw",
       "App_Icon": "https://i.ytimg.com/vi/eeoRqyhzRcw/hqdefault.jpg"
     },
@@ -160,7 +160,7 @@
     },
     {
       "Id": 26,
-      "App_Name": "BABA YAGA - Une histoire captivante de la campagne russe | Livre Audio + Texte | Contes et légendes",
+      "App_Name": "BABA YAGA - Une histoire captivante de la campagne russe",
       "URL": "https://www.youtube.com/watch?v=pKkmB6JLxmE",
       "App_Icon": "https://i.ytimg.com/vi/pKkmB6JLxmE/hqdefault.jpg"
     },
@@ -178,259 +178,259 @@
     },
     {
       "Id": 29,
-      "App_Name": "Le Petit Chaperon rouge - Charles Perrault - Livre Audio Français + Texte",
+      "App_Name": "Le Petit Chaperon rouge - Charles Perrault",
       "URL": "https://www.youtube.com/watch?v=4y0-32u04i0",
       "App_Icon": "https://i.ytimg.com/vi/4y0-32u04i0/hqdefault.jpg"
     },
     {
       "Id": 30,
-      "App_Name": "33- LA RECETTE DU SOLDAT - contes populaires slaves",
+      "App_Name": "33- LA RECETTE DU SOLDAT",
       "URL": "https://www.youtube.com/watch?v=_1cr82OFgJw",
       "App_Icon": "https://i.ytimg.com/vi/_1cr82OFgJw/hqdefault.jpg"
     },
     {
       "Id": 31,
-      "App_Name": "30- LE NIGAUD - contes populaires slaves",
+      "App_Name": "30- LE NIGAUD",
       "URL": "https://www.youtube.com/watch?v=hXNG137SL8c",
       "App_Icon": "https://i.ytimg.com/vi/hXNG137SL8c/hqdefault.jpg"
     },
     {
       "Id": 32,
-      "App_Name": "29- LE CHAT ET LE RENARD - contes populaires slaves",
+      "App_Name": "29- LE CHAT ET LE RENARD",
       "URL": "https://www.youtube.com/watch?v=oTLXEuWhhCU",
       "App_Icon": "https://i.ytimg.com/vi/oTLXEuWhhCU/hqdefault.jpg"
     },
     {
       "Id": 33,
-      "App_Name": "28 - LE RENARD ET LE LOUP - contes populaires slaves",
+      "App_Name": "28 - LE RENARD ET LE LOUP",
       "URL": "https://www.youtube.com/watch?v=u_2Q74EdI4U",
       "App_Icon": "https://i.ytimg.com/vi/u_2Q74EdI4U/hqdefault.jpg"
     },
     {
       "Id": 34,
-      "App_Name": "25 - LES DEUX FRÈRES - contes populaires slaves",
+      "App_Name": "25 - LES DEUX FRÈRES",
       "URL": "https://www.youtube.com/watch?v=GaDPQfD2HD8",
       "App_Icon": "https://i.ytimg.com/vi/GaDPQfD2HD8/hqdefault.jpg"
     },
     {
       "Id": 35,
-      "App_Name": "24 - L’AUMÔNE - contes populaires slaves",
+      "App_Name": "24 - L’AUMÔNE",
       "URL": "https://www.youtube.com/watch?v=W67rzTXzPtI",
       "App_Icon": "https://i.ytimg.com/vi/W67rzTXzPtI/hqdefault.jpg"
     },
     {
       "Id": 36,
-      "App_Name": "22 - NE FORÇONS POINT NOTRE TALENT - contes populaires slaves",
+      "App_Name": "22 - NE FORÇONS POINT NOTRE TALENT",
       "URL": "https://www.youtube.com/watch?v=0Dwgxq3_VhU",
       "App_Icon": "https://i.ytimg.com/vi/0Dwgxq3_VhU/hqdefault.jpg"
     },
     {
       "Id": 37,
-      "App_Name": "21 - L’HEUREUX BERGER - Recueil de contes populaires slaves",
+      "App_Name": "21 - L’HEUREUX BERGER ",
       "URL": "https://www.youtube.com/watch?v=4g1kXNag-Z0",
       "App_Icon": "https://i.ytimg.com/vi/4g1kXNag-Z0/hqdefault.jpg"
     },
     {
       "Id": 38,
-      "App_Name": "19 - L’ŒIL QUI RIT ET L’ŒIL QUI PLEURE OU LE RENARD BOITEUX  - contes populaires slaves",
+      "App_Name": "19 - L’ŒIL QUI RIT ET L’ŒIL QUI PLEURE OU LE RENARD BOITEUX ",
       "URL": "https://www.youtube.com/watch?v=BIpE1LJRna8",
       "App_Icon": "https://i.ytimg.com/vi/BIpE1LJRna8/hqdefault.jpg"
     },
     {
       "Id": 39,
-      "App_Name": "18 - LE LOUP NIGAUD - contes populaires slaves",
+      "App_Name": "18 - LE LOUP NIGAUD",
       "URL": "https://www.youtube.com/watch?v=1zmGepJGIOc",
       "App_Icon": "https://i.ytimg.com/vi/1zmGepJGIOc/hqdefault.jpg"
     },
     {
       "Id": 40,
-      "App_Name": "16 - LE POISSON D’OR - contes populaires slaves",
+      "App_Name": "16 - LE POISSON D’OR",
       "URL": "https://www.youtube.com/watch?v=z33v1dAXfsQ",
       "App_Icon": "https://i.ytimg.com/vi/z33v1dAXfsQ/hqdefault.jpg"
     },
     {
       "Id": 41,
-      "App_Name": "20 - LA BAVARDE - contes populaires slaves",
+      "App_Name": "20 - LA BAVARDE",
       "URL": "https://www.youtube.com/watch?v=l-tzHiQEz5g",
       "App_Icon": "https://i.ytimg.com/vi/l-tzHiQEz5g/hqdefault.jpg"
     },
     {
       "Id": 42,
-      "App_Name": "11 - LE LANGAGE  DES ANIMAUX - contes populaires slaves",
+      "App_Name": "11 - LE LANGAGE  DES ANIMAUX",
       "URL": "https://www.youtube.com/watch?v=Db_hxihptcE",
       "App_Icon": "https://i.ytimg.com/vi/Db_hxihptcE/hqdefault.jpg"
     },
     {
       "Id": 43,
-      "App_Name": "10 - LA BABA IAGA - contes populaires slaves",
+      "App_Name": "10 - LA BABA IAGA",
       "URL": "https://www.youtube.com/watch?v=ajtux9udVK4",
       "App_Icon": "https://i.ytimg.com/vi/ajtux9udVK4/hqdefault.jpg"
     },
     {
       "Id": 44,
-      "App_Name": "9 - BLANCHE NEIGE - contes populaires slaves",
+      "App_Name": "9 - BLANCHE NEIGE",
       "URL": "https://www.youtube.com/watch?v=VNuq7Ha1E8A",
       "App_Icon": "https://i.ytimg.com/vi/VNuq7Ha1E8A/hqdefault.jpg"
     },
     {
       "Id": 45,
-      "App_Name": "8 - LE PRINCE INESPÉRÉ - contes populaires slaves",
+      "App_Name": "8 - LE PRINCE INESPÉRÉ",
       "URL": "https://www.youtube.com/watch?v=EFISFLpiZSo",
       "App_Icon": "https://i.ytimg.com/vi/EFISFLpiZSo/hqdefault.jpg"
     },
     {
       "Id": 46,
-      "App_Name": "7 - LE BERGER ET LE DRAGON  - Contes populaires slaves",
+      "App_Name": "7 - LE BERGER ET LE DRAGON ",
       "URL": "https://www.youtube.com/watch?v=nexTFMgSU68",
       "App_Icon": "https://i.ytimg.com/vi/nexTFMgSU68/hqdefault.jpg"
     },
     {
       "Id": 47,
-      "App_Name": "6 - LA GELÉE LE SOLEIL ET LE VENT - contes populaires slaves",
+      "App_Name": "6 - LA GELÉE LE SOLEIL ET LE VENT",
       "URL": "https://www.youtube.com/watch?v=zltIr3OrX7s",
       "App_Icon": "https://i.ytimg.com/vi/zltIr3OrX7s/hqdefault.jpg"
     },
     {
       "Id": 48,
-      "App_Name": "1- UNE DRACHME DE LANGUE - contes populaires slaves",
+      "App_Name": "1- UNE DRACHME DE LANGUE",
       "URL": "https://www.youtube.com/watch?v=Ibe_mp6X9BQ",
       "App_Icon": "https://i.ytimg.com/vi/Ibe_mp6X9BQ/hqdefault.jpg"
     },
     {
       "Id": 49,
-      "App_Name": "2- LA TABLE, LA MUSETTE ET LE SAC - contes populaires slaves",
+      "App_Name": "2- LA TABLE, LA MUSETTE ET LE SAC",
       "URL": "https://www.youtube.com/watch?v=mWq5dG6XOw4",
       "App_Icon": "https://i.ytimg.com/vi/mWq5dG6XOw4/hqdefault.jpg"
     },
     {
       "Id": 50,
-      "App_Name": "3 - LE PETIT POUCET RUSSE - contes populaires slaves",
+      "App_Name": "3 - LE PETIT POUCET RUSSE",
       "URL": "https://www.youtube.com/watch?v=N66siE6dvec",
       "App_Icon": "https://i.ytimg.com/vi/N66siE6dvec/hqdefault.jpg"
     },
     {
       "Id": 51,
-      "App_Name": "4 - LA MAUVAISE FEMME  - contes populaires slaves",
+      "App_Name": "4 - LA MAUVAISE FEMME ",
       "URL": "https://www.youtube.com/watch?v=eY6W3jyGo1A",
       "App_Icon": "https://i.ytimg.com/vi/eY6W3jyGo1A/hqdefault.jpg"
     },
     {
       "Id": 52,
-      "App_Name": "1- L’Appel de Cthulhu  - Chapitre I - H P Lovecraft -  Texte + Livre  Audio français",
+      "App_Name": "1- L’Appel de Cthulhu  - Chapitre I - H P Lovecraft ",
       "URL": "https://www.youtube.com/watch?v=tSabXBm1UrY",
       "App_Icon": "https://i.ytimg.com/vi/tSabXBm1UrY/hqdefault.jpg"
     },
     {
       "Id": 53,
-      "App_Name": "2- L’Appel de Cthulhu - Chapitre II- H.P. Lovecraft - Texte + Livre Audio français",
+      "App_Name": "2- L’Appel de Cthulhu - Chapitre II- H.P. Lovecraft ",
       "URL": "https://www.youtube.com/watch?v=DrJN3fCsqgc",
       "App_Icon": "https://i.ytimg.com/vi/DrJN3fCsqgc/hqdefault.jpg"
     },
     {
       "Id": 54,
-      "App_Name": "3 - L’Appel de Cthulhu  - Chapitre III - H P  Lovecraft  - Texte + Livre  Audio français",
+      "App_Name": "3 - L’Appel de Cthulhu  - Chapitre III - H P  Lovecraft  ",
       "URL": "https://www.youtube.com/watch?v=ztK0Y2OuHz8",
       "App_Icon": "https://i.ytimg.com/vi/ztK0Y2OuHz8/hqdefault.jpg"
     },
     {
       "Id": 55,
-      "App_Name": "1- Le Journal d'une femme de chambre - Chapitre I - Texte + Livre  Audio français",
+      "App_Name": "1- Le Journal d'une femme de chambre - Chapitre I ",
       "URL": "https://www.youtube.com/watch?v=lJQyZkzhTpw",
       "App_Icon": "https://i.ytimg.com/vi/lJQyZkzhTpw/hqdefault.jpg"
     },
     {
       "Id": 56,
-      "App_Name": "2- Le Journal  d’une femme  de chambre  - CHAPITRE II - Texte + Livre Audio Français",
+      "App_Name": "2- Le Journal  d’une femme  de chambre  - CHAPITRE II ",
       "URL": "https://www.youtube.com/watch?v=d63dcvPORlc",
       "App_Icon": "https://i.ytimg.com/vi/d63dcvPORlc/hqdefault.jpg"
     },
     {
       "Id": 57,
-      "App_Name": "3- Le Journal  d’une femme  de chambre  - CHAPITRE III -  Texte + Livre  Audio français",
+      "App_Name": "3- Le Journal  d’une femme  de chambre  - CHAPITRE III ",
       "URL": "https://www.youtube.com/watch?v=nvDRy0pl74U",
       "App_Icon": "https://i.ytimg.com/vi/nvDRy0pl74U/hqdefault.jpg"
     },
     {
       "Id": 58,
-      "App_Name": "4 - Le Journal d'une femme de chambre - Chapitre IV - Texte + Livre  Audio français",
+      "App_Name": "4 - Le Journal d'une femme de chambre - Chapitre IV ",
       "URL": "https://www.youtube.com/watch?v=IpIlUBmKmuw",
       "App_Icon": "https://i.ytimg.com/vi/IpIlUBmKmuw/hqdefault.jpg"
     },
     {
       "Id": 59,
-      "App_Name": "5 - Le Journal d'une femme de chambre - Chapitre V - Texte + Livre  Audio français",
+      "App_Name": "5 - Le Journal d'une femme de chambre - Chapitre V ",
       "URL": "https://www.youtube.com/watch?v=UUkvtadgJGA",
       "App_Icon": "https://i.ytimg.com/vi/UUkvtadgJGA/hqdefault.jpg"
     },
     {
       "Id": 60,
-      "App_Name": "6 - Le Journal d'une femme de chambre  - Chapitre VI - Texte + Livre  Audio français",
+      "App_Name": "6 - Le Journal d'une femme de chambre  - Chapitre VI ",
       "URL": "https://www.youtube.com/watch?v=3mfp5UQiaV8",
       "App_Icon": "https://i.ytimg.com/vi/3mfp5UQiaV8/hqdefault.jpg"
     },
     {
       "Id": 61,
-      "App_Name": "7 -  Le Journal d'une femme de chambre  - Chapitre VII  - Texte + Livre  Audio français",
+      "App_Name": "7 -  Le Journal d'une femme de chambre  - Chapitre VII  ",
       "URL": "https://www.youtube.com/watch?v=7YnKATPpNqo",
       "App_Icon": "https://i.ytimg.com/vi/7YnKATPpNqo/hqdefault.jpg"
     },
     {
       "Id": 62,
-      "App_Name": "8 - Le Journal d'une femme de chambre  - Chapitre VIII  - Texte + Livre  Audio français",
+      "App_Name": "8 - Le Journal d'une femme de chambre  - Chapitre VIII  ",
       "URL": "https://www.youtube.com/watch?v=JrhguLHCRDs",
       "App_Icon": "https://i.ytimg.com/vi/JrhguLHCRDs/hqdefault.jpg"
     },
     {
       "Id": 63,
-      "App_Name": "9 - Le Journal d'une femme de chambre  - Chapitre XI - Texte + Livre  Audio français",
+      "App_Name": "9 - Le Journal d'une femme de chambre  - Chapitre XI ",
       "URL": "https://www.youtube.com/watch?v=ANj_hwG69fY",
       "App_Icon": "https://i.ytimg.com/vi/ANj_hwG69fY/hqdefault.jpg"
     },
     {
       "Id": 64,
-      "App_Name": "10 - Le Journal d'une femme de chambre - Chapitre X  - Texte + Livre  Audio français",
+      "App_Name": "10 - Le Journal d'une femme de chambre - Chapitre X  ",
       "URL": "https://www.youtube.com/watch?v=rAxhPjU-EHs",
       "App_Icon": "https://i.ytimg.com/vi/rAxhPjU-EHs/hqdefault.jpg"
     },
     {
       "Id": 65,
-      "App_Name": "11 -  Le Journal d'une femme de chambre  - Chapitre XI -  Texte + Livre  Audio français",
+      "App_Name": "11 -  Le Journal d'une femme de chambre  - Chapitre XI ",
       "URL": "https://www.youtube.com/watch?v=H3auer5Ij8w",
       "App_Icon": "https://i.ytimg.com/vi/H3auer5Ij8w/hqdefault.jpg"
     },
     {
       "Id": 66,
-      "App_Name": "12 - Le Journal d'une femme de chambre  - Chapitre XII - Texte + Livre  Audio français",
+      "App_Name": "12 - Le Journal d'une femme de chambre  - Chapitre XII ",
       "URL": "https://www.youtube.com/watch?v=PfwyFGY_3jI",
       "App_Icon": "https://i.ytimg.com/vi/PfwyFGY_3jI/hqdefault.jpg"
     },
     {
       "Id": 67,
-      "App_Name": "13 - Le Journal d'une femme de chambre - Chapitre XIII - Texte + Livre  Audio français",
+      "App_Name": "13 - Le Journal d'une femme de chambre - Chapitre XIII ",
       "URL": "https://www.youtube.com/watch?v=fFLZLWX8NcM",
       "App_Icon": "https://i.ytimg.com/vi/fFLZLWX8NcM/hqdefault.jpg"
     },
     {
       "Id": 68,
-      "App_Name": "14 - Le Journal d'une femme de chambre - Chapitre XIV - Texte + Livre  Audio français",
+      "App_Name": "14 - Le Journal d'une femme de chambre - Chapitre XIV ",
       "URL": "https://www.youtube.com/watch?v=79sAzgktwVM",
       "App_Icon": "https://i.ytimg.com/vi/79sAzgktwVM/hqdefault.jpg"
     },
     {
       "Id": 69,
-      "App_Name": "15 - Le Journal d'une femme de chambre - Chapitre XV - Texte + Livre  Audio français",
+      "App_Name": "15 - Le Journal d'une femme de chambre - Chapitre XV ",
       "URL": "https://www.youtube.com/watch?v=pfzWS5H3E9I",
       "App_Icon": "https://i.ytimg.com/vi/pfzWS5H3E9I/hqdefault.jpg"
     },
     {
       "Id": 70,
-      "App_Name": "16 - Le Journal d'une femme de chambre  - Chapitre XVI - Texte + Livre  Audio français",
+      "App_Name": "16 - Le Journal d'une femme de chambre  - Chapitre XVI ",
       "URL": "https://www.youtube.com/watch?v=eZNBluPBSi8",
       "App_Icon": "https://i.ytimg.com/vi/eZNBluPBSi8/hqdefault.jpg"
     },
     {
       "Id": 71,
-      "App_Name": "17 - Le Journal d'une femme de chambre - Chapitre XVII - Texte + Livre  Audio français",
+      "App_Name": "17 - Le Journal d'une femme de chambre - Chapitre XVII ",
       "URL": "https://www.youtube.com/watch?v=duBHhx7vgJM",
       "App_Icon": "https://i.ytimg.com/vi/duBHhx7vgJM/hqdefault.jpg"
     }
